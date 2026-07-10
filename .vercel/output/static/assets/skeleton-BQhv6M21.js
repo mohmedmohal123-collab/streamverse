0b80f941c0a0da9d0880d6cf26512fba4abb4599
mohmedmohal123-collab <mohmed.mohal123@gmail.com>
2026-07-10 16:25:24 +0300

    Updated project.json

---
+++ .vercel/output/static/assets/skeleton-BQhv6M21.js
@@ -0,0 +1,14 @@
+import { j as jsxRuntimeExports, t as cn } from "./index-B4P1PGaK.js";
+function Skeleton({ className, ...props }) {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(
+    "div",
+    {
+      "data-slot": "skeleton",
+      className: cn("bg-accent animate-pulse rounded-md", className),
+      ...props
+    }
+  );
+}
+export {
+  Skeleton as S
+};
